68b7630ffa7e330a223c4b9cc0fc50ea3dbabf3c
sunrc <sunrc@218.38.137.28>
2018-08-20 15:03:14 +0900

    firstpage init

---
+++ first_page/src/main/java/com/sunrc/firstpage/FirstPageApplication.java
@@ -0,0 +1,12 @@
+package com.sunrc.firstpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPageApplication.class, args);
+	}
+}
